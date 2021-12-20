ba4746b09c5e66a079c9aa5cbbe5babd695ac8b9
Brandon Rothel <rothel.2@wright.edu>
2021-12-19 21:57:57 -0500

    Autoformatting and cont work on sync motor movement [Still broken]

--- SandTable.ino
+++ SandTable.ino
@@ -13,7 +13,7 @@ int thetaVal = 0;
 int rMax = 11000;
 int thetaMax = 13050;
 
-void setup() { 
+void setup() {
   pinMode(rEnable, OUTPUT);
   pinMode(thetaEnable, OUTPUT);
   pinMode(thetaStep, OUTPUT);
@@ -21,102 +21,136 @@ void setup() {
   pinMode(rStep, OUTPUT);
   pinMode(rDir, OUTPUT);
   pinMode(rLimit, INPUT_PULLUP);
-  
+
   Serial.begin(9600);
   Serial.println("Starting Serial...");
-  
+
   digitalWrite(rEnable, 0);
   digitalWrite(thetaEnable, 0);
   delayAmount = 1;
   Serial.println("Starting Calibration:");
 }
 
-int calibrateR(){
-  for(int i = 0; i<rMax; i++){
-    if (digitalRead(rLimit) == 0){
+int calibrateR() {
+  for (int i = 0; i < rMax; i++) {
+    if (digitalRead(rLimit) == 0) {
       delay(3); //This delay and double check, filters out noise from stepper motors
-      if (digitalRead(rLimit) == 0){
+      if (digitalRead(rLimit) == 0) {
         Serial.println("R Calibration Successful!");
-      stepR(rMax, 0);
-      rVal = 0;
-      return 1;
+        stepR(rMax, 0);
+        rVal = 0;
+        return 1;
       }
-      
+
     }
     stepR(1, 1);
     //Serial.println(digitalRead(rLimit));
   }
   Serial.println("Calibration Failed...");
-  while(1){};
+  while (1) {};
   return 0;
 }
 
-int stepR(int steps, int dir){
+int stepR(int steps, int dir) {
   digitalWrite(rDir, dir);
-  for(int i = 0; i<steps; i++){
-        if(rVal>=rMax || abs(rVal)>=rMax){
-          Serial.println("Out of bounds");
-          return;
-        }
-        digitalWrite(rStep, 1);
-        delay(delayAmount);
-        digitalWrite(rStep, 0);
-        delay(delayAmount);
-        if(dir){
-          rVal++;
-        } else {
-          rVal--;
-        }
-        //Serial.println("rVal = "+rVal);
-      }
+  for (int i = 0; i < steps; i++) {
+    if (rVal >= rMax || abs(rVal) >= rMax) {
+      Serial.println("Out of bounds");
+      return;
+    }
+    digitalWrite(rStep, 1);
+    delay(delayAmount);
+    digitalWrite(rStep, 0);
+    delay(delayAmount);
+    if (dir) {
+      rVal++;
+    } else {
+      rVal--;
+    }
+    //Serial.println("rVal = "+rVal);
+  }
 }
 
-int stepTheta(int steps, int dir){
+int stepTheta(int steps, int dir) {
   digitalWrite(thetaDir, dir);
-  for(int i = 0; i<steps; i++){
-        digitalWrite(thetaStep, 1);
-        delay(delayAmount);
-        digitalWrite(thetaStep, 0);
-        delay(delayAmount);
-        if(dir){
-          thetaVal++;
-        } else {
-          thetaVal--;
-        }
-      }
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(thetaStep, 1);
+    delay(delayAmount);
+    digitalWrite(thetaStep, 0);
+    delay(delayAmount);
+    if (dir) {
+      thetaVal++;
+    } else {
+      thetaVal--;
+    }
+  }
 }
 
-int goToPolar(int rValEnd, double thetaValEnd){
-  Serial.println("Starting at coords: ("+(String)rVal + ", "+(String)thetaVal+")");
-  Serial.println("Ending at coords:   ("+(String)rValEnd + ", "+(String)thetaValEnd+")");
+int goToPolar(int rValEnd, double thetaValEnd) {
+  Serial.println("Starting at coords: (" + (String)rVal + ", " + (String)thetaVal + ")");
+  Serial.println("Ending at coords:   (" + (String)rValEnd + ", " + (String)thetaValEnd + ")");
   int rValDir;
-  int distanceToR = abs(rVal-rValEnd);
-  if(rValEnd>rVal){
+  int distanceToR = abs(rVal - rValEnd);
+  if (rValEnd > rVal) {
     rValDir = 1;
   } else {
-    rValDir = 0;    
+    rValDir = 0;
   }
   int thetaValDir;
-  int distanceToTheta = abs(thetaVal-thetaValEnd);
-  if(thetaValEnd>thetaVal){
+  int distanceToTheta = abs(thetaVal - thetaValEnd);
+  if (thetaValEnd > thetaVal) {
     thetaValDir = 1;
   } else {
-    thetaValDir = 0;    
+    thetaValDir = 0;
+  }
+  /*//This ensures the theta motor chooses the shortest direction to the endpoint
+    if(distanceToTheta>(thetaMax/2)){
+    distanceToTheta = distanceToTheta-(thetaMax/2);
+    thetaValDir ^= thetaValDir;
+    }*/
+  //This attempts to alternate the motors so they appear to move at the same time
+  if (distanceToR > distanceToTheta) {
+    int turnRatio = distanceToR / distanceToTheta;    
+    for (int i = 0; i < (distanceToR / turnRatio); i++) {
+      stepR(turnRatio, rValDir);
+      stepTheta(1, thetaValDir);
+    }
+  } else {
+    int turnRatio = distanceToTheta / distanceToR;
+    for (int i = 0; i < (distanceToTheta / turnRatio); i++) {
+      stepTheta(turnRatio, thetaValDir);
+      stepR(1, rValDir);
+    }
+  }
+  Serial.println("Actual ending coords:   (" + (String)rVal + ", " + (String)thetaVal + ") (Before adjusting)");
+  //Below is to catch if we didn't make it all the way to both coords due to rounding
+  distanceToR = abs(rVal - rValEnd);
+  if (rValEnd > rVal) {
+    rValDir = 1;
+  } else {
+    rValDir = 0;
+  }
+  distanceToTheta = abs(thetaVal - thetaValEnd);
+  if (thetaValEnd > thetaVal) {
+    thetaValDir = 1;
+  } else {
+    thetaValDir = 0;
   }
   stepR(distanceToR, rValDir);
   stepTheta(distanceToTheta, thetaValDir);
+  Serial.println("Actual ending coords:   (" + (String)rVal + ", " + (String)thetaVal + ") (After adjusting)\n");
   return;
 }
 
 
-int spiral(int cw){
-  while (rVal < (rMax-50)){
-    stepTheta(10,cw);
-    stepR(1,1);
+int spiral(int cw) {
+  while (rVal < (rMax - 50)) {
+    stepTheta(10, cw);
+    stepR(1, 1);
   }
-  while (rVal > 0){
-    stepTheta(10,cw);
-    stepR(1,0);
+  while (rVal > 0) {
+    stepTheta(10, cw);
+    stepR(1, 0);
   }
 }
 
@@ -124,19 +158,18 @@ int spiral(int cw){
 void loop() {
   calibrateR();
 
-  goToPolar(0,0);
-  goToPolar(rMax/2, 0);
-  goToPolar(0,0);
-  goToPolar(rMax/2, thetaMax*0.25);
-  goToPolar(0,0);
-  goToPolar(rMax/2, thetaMax*0.50);
-  goToPolar(0,0);
-  goToPolar(rMax/2, thetaMax*0.75);
-  goToPolar(0,0);
-  goToPolar(rMax/2, 0);  
-  
+  goToPolar(0, 0);
+  goToPolar(rMax / 2, thetaMax * 0.25);
+  //goToPolar(0,0);
+  goToPolar(rMax / 2, thetaMax * 0.50);
+  //goToPolar(0,0);
+  goToPolar(rMax / 2, thetaMax * 0.75);
+  //goToPolar(0,0);
+  goToPolar(rMax / 2, 0);
+  //goToPolar(0,0);
+  goToPolar(rMax / 2, 0);
   //spiral(0);
   //spiral(1);
-  
-  while(1){}
+
+  while (1) {}
 }
